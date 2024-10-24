f1072355e45fed6b3fbfba97d9834496722cb968
aaronmaciias <aaronyariadna20@gmail.com>
2024-10-24 16:44:43 +0200

    Update nmaap.sh

--- nmaap.sh
+++ nmaap.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Descripción: Script que lista los puertos rápidamente y posteriormente los escanea a un nivel más complejo.
-# Fecha comienzo: 18 Octubre del 2024
-# Fecha de fin: x
-# Hecho por: Aarón Esteban Macías
-# Versión: 1.0
-# Basado en: https://github.com/Hackavis/nmapA/blob/main/nmapA.sh
+# Description: Script that quickly lists ports and then scans them at a more complex level.
+# Start date: October 18, 2024
+# End date: October 24, 2024
+# Created by: Aarón Esteban Macías
+# Version: 1.0
+# Based on: https://github.com/Hackavis/nmapA/blob/main/nmapA.sh
 
-# Gama de colores
-purpura="\033[0;35m"
-verde="\033[0;32m"
-rojo="\033[0;31m"
-fincolor="\033[0m"
-amarillo="\033[0;33m"
+# Color palette
+purple="\033[0;35m"
+green="\033[0;32m"
+red="\033[0;31m"
+resetcolor="\033[0m"
+yellow="\033[0;33m"
 
-# Verificar si se ejecuta con permisos de superusuario
+# Check if running with superuser privileges
 if [ "$EUID" -ne 0 ]; then 
-    echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-    echo -e "${rojo}[X] Ejecuta ${0} con sudo o como root.${fincolor}"
-    echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+    echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+    echo -e "${red}[X] Run ${0} with sudo or as root.${resetcolor}"
+    echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
     exit 1
 fi
 
-# Verifica si está instalado nmap; si no está instalado, pregunta al usuario si desea instalarlo
+# Check if Nmap is installed; if not, ask the user if they want to install it
 if [[ -f /usr/bin/nmap || -d /usr/share/nmap ]]; then
-    echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
-    echo -e "${verde}[✓] Nmap esta instalado en su sistema${fincolor}"
-    echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
+    echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
+    echo -e "${green}[✓] Nmap is installed on your system.${resetcolor}"
+    echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
 else
-    # Si nmap no está instalado, pregunta si desea instalarlo
-    echo -e "${amarillo}-------------------------------------------------------------------------------------${fincolor}"
-    echo -e "${amarillo}[!] Nmap no está instalado.${fincolor} ${purpura}¿Quieres instalarlo? (s/n)${fincolor}"
-    echo -e "${amarillo}-------------------------------------------------------------------------------------${fincolor}"
-    read opcion
+    # If Nmap is not installed, ask if they want to install it
+    echo -e "${yellow}-------------------------------------------------------------------------------------${resetcolor}"
+    echo -e "${yellow}[!] Nmap is not installed.${resetcolor} ${purple}Do you want to install it? (y/n)${resetcolor}"
+    echo -e "${yellow}-------------------------------------------------------------------------------------${resetcolor}"
+    read option
 
-    if [[ $opcion == "s" ]]; then
-        # Detectar el sistema de paquetes y proceder con la instalación
+    if [[ $option == "y" ]]; then
+        # Detect the package manager and proceed with the installation
         if command -v apt &> /dev/null; then
             sudo apt update && sudo apt install -y nmap
         elif command -v yum &> /dev/null; then
@@ -45,64 +45,64 @@ else
         elif command -v pacman &> /dev/null; then
             sudo pacman -Syu nmap
         else
-            echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-            echo -e "${rojo}[X] No se pudo determinar el gestor de paquetes para instalar nmap.${fincolor}"
-            echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+            echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+            echo -e "${red}[X] Could not determine the package manager to install Nmap.${resetcolor}"
+            echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
             exit 1
         fi
-    elif [[ $opcion == "n" ]]; then
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-        echo -e "${rojo}[X] Instalación de Nmap cancelada.${fincolor}"
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+    elif [[ $option == "n" ]]; then
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${red}[X] Nmap installation canceled.${resetcolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
         exit 1 
     else
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-        echo -e "${rojo}[X] Opción no válida.${fincolor}${purpura}Por favor, elige 's' o 'n'.${fincolor}"
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${red}[X] Invalid option.${resetcolor}${purple}Please choose 'y' or 'n'.${resetcolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
         exit 1
     fi
 fi
  
 if [[ $# -eq 1 ]]; then
     if [[ "$1" =~ ^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then 
-        echo -e "${purpura}-------------------------------------------------------------------------------------${fincolor}"
-        echo -e "${purpura}[*] Reconocimiento inicial de puertos.${fincolor}"
-        echo -e "${purpura}-------------------------------------------------------------------------------------${fincolor}"
+        echo -e "${purple}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${purple}[*] Initial port reconnaissance.${resetcolor}"
+        echo -e "${purple}-------------------------------------------------------------------------------------${resetcolor}"
         ip=$1
         nmap -p- -sS --min-rate 5000 --open -Pn -v -n $ip -oG ports.tmp
-        echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
-        echo -e "${verde}[✓] Escaneo correctamente terminado.${fincolor}"
-        echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
+        echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${green}[✓] Scan successfully completed.${resetcolor}"
+        echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
     else
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-        echo -e "${rojo}[X] Porfavor, introduce una IPv4 correcta.${fincolor}"
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${red}[X] Please enter a valid IPv4 address.${resetcolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
         exit 1
     fi
 else
-echo -e "${amarillo}-------------------------------------------------------------------------------------${fincolor}"
-echo -e "${amarillo}[!] No has introducido ninguna IPv4, porfavor vuelva a intentarlo.${fincolor}"
-echo -e "${amarillo}-------------------------------------------------------------------------------------${fincolor}"
+echo -e "${yellow}-------------------------------------------------------------------------------------${resetcolor}"
+echo -e "${yellow}[!] No IPv4 address provided, please try again.${resetcolor}"
+echo -e "${yellow}-------------------------------------------------------------------------------------${resetcolor}"
 exit 1
 fi
 ports="$(cat ports.tmp | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
 if [[ $ports = "" ]]; then
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
-		echo -e "${rojo}[X] No se ha detectado ningún puerto abierto.${fincolor}"
-        echo -e "${rojo}-------------------------------------------------------------------------------------${fincolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
+        echo -e "${red}[X] No open ports detected.${resetcolor}"
+        echo -e "${red}-------------------------------------------------------------------------------------${resetcolor}"
 		rm ports.tmp
 		exit 1
 else
-    echo -e "${purpura}-------------------------------------------------------------------------------------${fincolor}"
-    echo -e "${purpura}[*] Escaneo avanzado de servicios${fincolor}" 
-    echo -e "${purpura}-------------------------------------------------------------------------------------${fincolor}"
-    nmap -sCV -p$ports $ip -oN InfoPuertos
-    sed -i '1,3d' InfoPuertos
-    echo -e "Dirección IP: $ip" >> InfoPuertos
-    echo -e "Puertos abiertos: $ports\n" >> InfoPuertos
+    echo -e "${purple}-------------------------------------------------------------------------------------${resetcolor}"
+    echo -e "${purple}[*] Advanced service scan${resetcolor}" 
+    echo -e "${purple}-------------------------------------------------------------------------------------${resetcolor}"
+    nmap -sCV -p$ports $ip -oN PortInfo
+    sed -i '1,3d' PortInfo
+    echo -e "IP Address: $ip" >> PortInfo
+    echo -e "Open ports: $ports\n" >> PortInfo
     rm ports.tmp
-    echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
-    echo -e "${verde}[✓] Escaneo completado, se ha generado el fichero InfoPuertos.${fincolor}" 
-    echo -e "${verde}-------------------------------------------------------------------------------------${fincolor}"
+    echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
+    echo -e "${green}[✓] Scan completed, the PortInfo file has been generated.${resetcolor}" 
+    echo -e "${green}-------------------------------------------------------------------------------------${resetcolor}"
     echo $ip | xclip -sel clip
 fi
